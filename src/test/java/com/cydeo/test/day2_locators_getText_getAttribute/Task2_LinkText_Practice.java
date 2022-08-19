a1fb0f301ffac02fde98f88385266c4bb065d03e
Turgay <tubas2334@gmail.com>
2022-08-19 12:54:27 +0300

    first commit

--- src/test/java/com/cydeo/test/day2_locators_getText_getAttribute/Task2_LinkText_Practice.java
+++ src/test/java/com/cydeo/test/day2_locators_getText_getAttribute/Task2_LinkText_Practice.java
@@ -1,4 +1,4 @@
-package com.cydeo.test.day1_Selenium_Intro;
+package com.cydeo.test.day2_locators_getText_getAttribute;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
