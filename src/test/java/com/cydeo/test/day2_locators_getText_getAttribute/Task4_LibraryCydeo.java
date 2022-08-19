af6bb454d6e2b0ec9313ff11133b6ec48d536ea8
Turgay <tubas2334@gmail.com>
2022-08-19 14:13:25 +0300

    first commit

---
+++ src/test/java/com/cydeo/test/day2_locators_getText_getAttribute/Task4_LibraryCydeo.java
@@ -0,0 +1,31 @@
+package com.cydeo.test.day2_locators_getText_getAttribute;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Task4_LibraryCydeo {
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https://library1.cydeo.com/login.html");
+
+        WebElement usernameInput = driver.findElement(By.className("form-control"));
+        usernameInput.sendKeys("incorrect@email.com");
+
+        WebElement passwordInput = driver.findElement(By.id("inputPassword"));
+        passwordInput.sendKeys("incorrect password");
+
+        WebElement signInButton = driver.findElement(By.tagName("button"));
+        signInButton.click();
+
+
+
+
+    }
+}
