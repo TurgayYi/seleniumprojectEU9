e4ecaa62ea41887772ce6504e178e50ed3baabd1
CYDEO <tubas2334@gmail.com>
2022-08-26 01:09:26 +0300

    created

---
+++ src/test/java/com/cydeo/test/day2_locators_getText_getAttribute/Homework3_BackAndForth.java
@@ -0,0 +1,65 @@
+package com.cydeo.test.day2_locators_getText_getAttribute;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Homework3_BackAndForth {
+
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://google.com");
+
+        driver.manage().window().fullscreen();
+
+        WebElement gmail = driver.findElement(By.className("gb_d"));
+        gmail.click();
+
+        String actualTitle = driver.getTitle();
+
+        if(actualTitle.contains("Gmail")){
+            System.out.println("Gmail title verification PASSED");
+        }else{
+            System.out.println("Gmail title verification FAILED");
+        }
+
+        driver.navigate().back();
+
+        String actualGoogleTitle = driver.getTitle();
+        String expectedGoogleTitle = "Google";
+
+        if(actualGoogleTitle.equals(expectedGoogleTitle)){
+            System.out.println("Google title verification PASSED");
+        }else{
+            System.out.println("Google title verification PASSED");
+        }
+
+        driver.close();
+
+
+
+
+
+
+    }
+
+
+
+}
+/*
+TC #3: Back and forth navigation
+1- Open a chrome browser
+2- Go to: https://google.com
+3- Click to Gmail from top right.
+4- Verify title contains:
+Expected: Gmail
+5- Go back to Google by using the .back();
+6- Verify title equals:
+Expected: Google
+ */
---
+++ src/test/java/com/cydeo/test/day2_locators_getText_getAttribute/Homework4_PractiseCydeo.java
@@ -0,0 +1,50 @@
+package com.cydeo.test.day2_locators_getText_getAttribute;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Homework4_PractiseCydeo {
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://practice.cydeo.com/inputs");
+
+        WebElement home = driver.findElement(By.className("nav-link"));
+
+        home.click();
+
+        String actualHomeTitle = driver.getTitle();
+        String expectedHomeTitle = "Practice";
+
+        if(actualHomeTitle.equals(expectedHomeTitle)){
+            System.out.println("Home title verification PASSED");
+        }else{
+            System.out.println("Home title verification FAILED");
+        }
+
+        driver.close();
+
+
+
+
+    }
+}
+
+
+/*
+
+TC #4: Practice Cydeo – Class locator practice
+1- Open a chrome browser
+2- Go to: https://practice.cydeo.com/inputs
+3- Click to “Home” link
+4- Verify title is as expected:
+Expected: Practice
+PS: Locate “Home” link using “className” locator
+
+ */
