4870d3af72ea8e83f27b898b9ff3deb54cc94302
CYDEO <tubas2334@gmail.com>
2022-08-26 00:32:22 +0300

    created

---
+++ src/test/java/com/cydeo/test/day2_locators_getText_getAttribute/Homework1.java
@@ -0,0 +1,53 @@
+package com.cydeo.test.day2_locators_getText_getAttribute;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Homework1 {
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+
+
+        driver.get("https://www.etsy.com");
+        driver.manage().window().maximize();
+
+        WebElement element = driver.findElement(By.id("global-enhancements-search-query"));
+        element.sendKeys("wooden spoon" + Keys.ENTER);
+
+        String actualTitle = driver.getTitle();
+        String expectedTitle = "Wooden spoon - Etsy";
+
+        if(actualTitle.equals(expectedTitle)){
+            System.out.println("Test verification PASSED");
+        }else{
+            System.out.println("Test verification FAILED");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+/*
+TC #1: Etsy Title Verification
+1. Open Chrome browser
+2. Go to https://www.etsy.com
+3. Search for “wooden spoon”
+4. Verify title:
+Expected: “Wooden spoon | Etsy
+ */
---
+++ src/test/java/com/cydeo/test/day2_locators_getText_getAttribute/Homework2_ZeroBank.java
@@ -0,0 +1,41 @@
+package com.cydeo.test.day2_locators_getText_getAttribute;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Homework2_ZeroBank {
+
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://zero.webappsecurity.com/login.html");
+
+        WebElement headerText = driver.findElement(By.className("page-header"));
+
+       String actualHeaderText =  headerText.getText();
+       String expectedHeaderText = "Log in to ZeroBank";
+
+        if(actualHeaderText.equals(expectedHeaderText)) {
+            System.out.println("Header text verification PASSED");
+
+        }else{
+            System.out.println("Header text verification FAILED");
+        }
+
+
+
+    }
+
+}
+/*
+TC #2: Zero Bank header verification
+1. Open Chrome browser
+2. Go to http://zero.webappsecurity.com/login.html
+3. Verify header text
+Expected: “Log in to ZeroBank
+ */
