52e668a5c4f6dffeacd5102b59adb46ef8da0616
CYDEO <tubas2334@gmail.com>
2022-09-09 15:16:27 +0300

    updated

---
+++ src/test/java/com/cydeo/utilities/WebTableUtils.java
@@ -0,0 +1,31 @@
+package com.cydeo.utilities;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class WebTableUtils {
+
+    //TC #2: Web table practice Task1 cont.
+    //Method #1 info:
+    //• Name: returnOrderDate ()
+    //• Return type: String
+    //• Arg1: WebDriver driver
+    //• Arg2: String costumerName
+    //This method should accept a costumerName and return the costumer order date
+    //as a String.
+
+    public static String returnOrderDate (WebDriver driver,String customerName){
+
+        String locator = "//td[.='"+customerName+"']/following-sibling::td[3]";
+
+        WebElement customerDateCell = driver.findElement(By.xpath(locator));
+
+        return customerDateCell.getText();
+
+    }
+
+
+
+
+}
