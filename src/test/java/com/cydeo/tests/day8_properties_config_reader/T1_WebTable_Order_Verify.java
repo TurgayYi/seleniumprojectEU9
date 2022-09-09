b1ce962107e60eb83c3e49a55c78e97763e6dfe9
CYDEO <tubas2334@gmail.com>
2022-09-09 12:35:11 +0300

    updated

---
+++ src/test/java/com/cydeo/tests/day8_properties_config_reader/T1_WebTable_Order_Verify.java
@@ -0,0 +1,24 @@
+package com.cydeo.tests.day8_properties_config_reader;
+
+import org.testng.annotations.Test;
+
+public class T1_WebTable_Order_Verify {
+
+
+    @Test
+    public void order_name_verify_test(){
+
+        //TC #1: Web table practice
+        //1. Go to: https://practice.cydeo.com/web-tables
+        //2. Verify Bob’s name is listed as expected.
+        //Expected: “Bob Martin”
+        //3. Verify Bob Martin’s order date is as expected
+        //Expected: 12/31/2021
+
+
+
+    }
+
+
+
+}
