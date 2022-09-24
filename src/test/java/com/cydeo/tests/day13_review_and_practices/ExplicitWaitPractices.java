c7612f88b98543696690acf61ffc02451c0d5095
CYDEO <tubas2334@gmail.com>
2022-09-24 18:10:47 +0300

    updated

---
+++ src/test/java/com/cydeo/tests/day13_review_and_practices/ExplicitWaitPractices.java
@@ -0,0 +1,28 @@
+package com.cydeo.tests.day13_review_and_practices;
+
+import com.cydeo.utilities.Driver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class ExplicitWaitPractices {
+
+
+    @BeforeMethod
+
+    public void setUpMethod(){
+        Driver.getDriver().get("https://practice.cydeo.com/dynamic_controls");
+    }
+
+    @Test
+    public void remove_button_test(){
+
+
+
+
+    }
+
+
+
+
+
+}
