1ac605399cab8126e3044d6709d191cc10873073
TurgayYi <tubas2334@gmail.com>
2022-09-24 12:28:58 +0300

    updated

--- src/test/java/com/cydeo/pages/DynamicLoad7Page.java
+++ src/test/java/com/cydeo/pages/DynamicLoad7Page.java
@@ -1,35 +1,20 @@
 package com.cydeo.pages;
-
-import com.cydeo.utilities.Driver;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
-
+import com.cydeo.utilities.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.*;
 public class DynamicLoad7Page {
 
-    //Create constructor and instantiate driver and object of the class
+
+    //#1-Create constructor and instantiate driver and object of the class
+
     public DynamicLoad7Page(){
-
-        PageFactory.initElements(Driver.getDriver(),   this);
-
+        PageFactory.initElements(Driver.getDriver(), this);
     }
 
-    @FindBy(xpath = "//div[@id='alert']//strong")
+    @FindBy(xpath = "//strong[.='Done!']")
     public WebElement doneMessage;
-
 
     @FindBy(xpath = "//img[@alt='square pants']")
     public WebElement spongeBobImage;
-
-
-
-
-
-
-
-
-
-
 
 }
